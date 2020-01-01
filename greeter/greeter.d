63c523d486188cd9928afb57c8fc91e58f6c7d68
David Young-Chan Kay <davidykay@gmail.com>
2019-12-31 19:36:39 -0800

    Initial import

---
+++ greeter/greeter.d
@@ -0,0 +1,21 @@
+module greeter;
+
+import std.stdio;
+import std.string;
+
+class Greeter {
+ private string greeting;
+
+ public:
+  this(in string greeting) {
+    this.greeting = greeting.dup;
+  }
+
+  string makeGreeting(in immutable string thing) {
+    return format("%s %s!", this.greeting, thing);
+  }
+
+  void greet(in immutable string thing) {
+    writeln(makeGreeting(thing));
+  }
+}
---
+++ hello/hello.cpp
@@ -0,0 +1,19 @@
+// #include "examples/cpp/hello-lib.h"
+#include <string>
+#include <iostream>
+#include <stdio.h>
+
+// using hello::HelloLib;
+using std::string;
+
+int main(int argc, char** argv) {
+  // HelloLib lib("Hello");
+  //string thing = "world";
+  //if (argc > 1) {
+  //  thing = argv[1];
+  //}
+  //lib.greet(thing);
+  //
+  printf("Hello world!\n");
+  return 0;
+}
